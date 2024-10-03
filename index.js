6a983bb753825fa102b81d2375f8ac9afc03271a
Sean Graham <seangraham006@gmail.com>
2024-10-03 14:14:56 +0000

    created users and exercises schemas

--- index.js
+++ index.js
@@ -21,6 +21,20 @@ mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log('Connected to MongoDB Atlas'))
   .catch(err => console.error('Failed to connect:', err));
 
+const UsersSchema = new mongoose.Schema({
+  username: {type: String, required: true}
+});
+
+const ExercisesSchema = new mongoose.Schema({
+  user_id: {type: String, required: true},
+  description: {type: String, required: true},
+  duration: {type: Number, required: true},
+  date: {type: Date, required: false}
+});
+
+const User = mongoose.model('User',UsersSchema);
+const Exercise = mongoose.model('Exercise',ExercisesSchema);
+
 //////////////////////////////////////////////////////////////////////////////////////////
 
 const listener = app.listen(process.env.PORT || 3000, () => {
