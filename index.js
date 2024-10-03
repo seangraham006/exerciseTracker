bd2ac073dfdff5beee752e6551c83f308c30b263
Sean Graham <seangraham006@gmail.com>
2024-10-03 13:50:33 +0000

    added packages mongoose, nanoid and express.json

--- index.js
+++ index.js
@@ -1,17 +1,21 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
-require('dotenv').config()
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const mongoose = require('mongoose');
+require('dotenv').config();
+let nanoid;
+(async () => {
+  nanoid = (await import('nanoid')).nanoid;
+})();
 
-app.use(cors())
-app.use(express.static('public'))
+app.use(cors());
+app.use(express.static('public'));
+app.use(express.json());
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
 
-
-
-
+//////////////////////////////////////////////////////////////////////////////////////////
 
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log('Your app is listening on port ' + listener.address().port)
